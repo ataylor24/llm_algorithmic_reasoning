6f63eb4b6659332a994d8c9de4142089abbaef4c
anthony-c-cuturrufo <anthonycuturrufo1@gmail.com>
2024-05-20 15:35:45 -0700

    Add Floyd Warshall functionality

--- data_generation/data_utils.py
+++ data_generation/data_utils.py
@@ -44,7 +44,12 @@ FORMATTED_ALGORITHMS = {
         "name": "Bellman-Ford",
         "goal": "shortest path",
         "instruction": ""
-        }
+        },
+    "floyd_warshall": {
+        "name": "Floyd-Warshall",
+        "goal": "shortest-path",
+        "instruction": "Output the final predecessor matrix"
+    }
 }
 
 TRAIN_TEST_SPLIT = {
@@ -92,7 +97,10 @@ def write_llama_chat_format(output_dir, data_sect, data):
                 hints = execution_step + "\n" if (reasoning_strategy != "IO" and data_sect != "testing") or i % 2 == 1 else ""
 
                 if i == 0:
-                    content = f"Please perform the {FORMATTED_ALGORITHMS[algorithm]['name']} algorithm for {FORMATTED_ALGORITHMS[algorithm]['goal']} on the following graph: Edgelist: [{','.join([str(tuple(edge)) for edge in edge_list])}]; Source Node: {str(source_node)}\n{hints}\n{FORMATTED_ALGORITHMS[algorithm]['instruction']}"
+                    if source_node == "":
+                        content = f"Please perform the {FORMATTED_ALGORITHMS[algorithm]['name']} algorithm for {FORMATTED_ALGORITHMS[algorithm]['goal']} on the following graph: Edgelist: [{','.join([str(tuple(edge)) for edge in edge_list])}]; \n{hints}\n{FORMATTED_ALGORITHMS[algorithm]['instruction']}"
+                    else:
+                        content = f"Please perform the {FORMATTED_ALGORITHMS[algorithm]['name']} algorithm for {FORMATTED_ALGORITHMS[algorithm]['goal']} on the following graph: Edgelist: [{','.join([str(tuple(edge)) for edge in edge_list])}]; Source Node: {str(source_node)}\n{hints}\n{FORMATTED_ALGORITHMS[algorithm]['instruction']}"
                 elif i + 1 >= len(data[idx]["hints"]):
                     content =  data[idx]["outputs"]
                 elif i % 2 == 1:
@@ -115,30 +123,33 @@ def write_llama_chat_format(output_dir, data_sect, data):
         write_data_config_readme(out_yml)
         
         write_json(outfile + ".json", llama_data)
-        
+
 def write_llama_format(output_dir, data_sect, data):
     for hint_level in REASONING_STRATEGIES:
         llama_data = []
-            
         for idx in data:
-            
             algorithm = data[idx]["inputs"][0]
             edge_list = data[idx]["inputs"][1]
             source_node = data[idx]["inputs"][2]
-                        
-            
-            llama_data_inst = {
+
+            if source_node == "":
+                llama_data_inst = {
                 "instruction": f"Please perform the {FORMATTED_ALGORITHMS[algorithm]['name']} algorithm for {FORMATTED_ALGORITHMS[algorithm]['goal']} on the following graph:",
-                "input": f"Edgelist: [{','.join([str(tuple(edge)) for edge in edge_list])}]; Source Node: {str(source_node)}.",
+                "input": f"Edgelist: [{','.join([str(tuple(edge)) for edge in edge_list])}];.",
                 "output": data[idx]["outputs"]
-            }
+                }
+            else:
+                llama_data_inst = {
+                    "instruction": f"Please perform the {FORMATTED_ALGORITHMS[algorithm]['name']} algorithm for {FORMATTED_ALGORITHMS[algorithm]['goal']} on the following graph:",
+                    "input": f"Edgelist: [{','.join([str(tuple(edge)) for edge in edge_list])}]; Source Node: {str(source_node)}.",
+                    "output": data[idx]["outputs"]
+                }
             if hint_level != "no_hints" and data_sect != "testing":
                 llama_data_inst["hints"] = data[idx]["hints"]
 
             llama_data.append(llama_data_inst)  
-        
+
         outfile = os.path.join(os.path.join(output_dir,hint_level), data_sect)
-        
         write_json(outfile + ".json", llama_data)
     
 def resolve_output_dirs(output_dir, algorithm, output_formats, graph_size):
@@ -190,7 +201,7 @@ def parse_args():
     parser.add_argument("-num_samples", "--num_samples", type=int, default=-1, help="Number of data samples to generate.")
     parser.add_argument("-neg_edges", "--neg_edges", type=bool, default=True, help="Include negative edges, ex. '0 is not reachable from 1'.")
     parser.add_argument("-seed", "--seed", type=int, default=100898, help="Random seed used in constructing the CLRS sampler; the default is 10081998.")
-    parser.add_argument("-output_dir", "--output_dir", type=str, default="/local2/ataylor2/algorithmic_reasoning", help="Output directory. Will create folders named after the algorithm for which data is generated.")
+    parser.add_argument("-output_dir", "--output_dir", type=str, default="/local2/acc/algorithmic_reasoning", help="Output directory. Will create folders named after the algorithm for which data is generated.")
     parser.add_argument("-train_test_split", "--train_test_split", type=list, default=[1000,500], help="Training/Testing split ratios. The Test set will be equally split into Validation and Test.")
     parser.add_argument("-output_formats", "--output_formats", type=list, default=["llama2", "mistral7b"], choices=OUTPUT_FORMATS, help="Output format for dataset")
     # Parse the arguments
